bea4a2c7974e0cb5ce8bb92b3f1f8ac26dd02e8b
Gianfranco Orlando <gianfranco19@hotmail.com>
2022-01-24 18:58:55 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,63 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Gianfranco Orlando" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0
 
-// selecting the element with an id of credit
+//getting html elements
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+let buttons = document.querySelectorAll('.button-container')
+const gbQty = document.getElementById('qty-gb')
+const ccQty = document.getElementById('qty-cc')
+const sugarQty = document.getElementById('qty-sugar')
+const totalQty = document.getElementById('qty-total')
 
-// Code to update name display
+//changine credential
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+//add listeners
+for (let x=0; x<buttons.length; x++) {
+    buttons[x].addEventListener('click', function(e) {
+        let btnId = e.target.id
+        switch(btnId) {
+            case 'add-gb':
+                gb++
+                gbQty.innerHTML = gb
+            break
+            case 'minus-gb':
+                gb--
+                if (gb<0) {gb = 0}
+                gbQty.innerHTML = gb
+            break
+            case 'add-cc':
+                cc++
+                ccQty.innerHTML = cc
+            break
+            case 'minus-cc':
+                cc--
+                if (cc<0) {cc = 0}
+                ccQty.innerHTML = cc
+            break
+            case 'add-sugar':
+                sugar++
+                sugarQty.innerHTML = sugar
+            break
+            case 'minus-sugar':
+                sugar--
+                if (sugar<0) {sugar = 0}
+                sugarQty.innerHTML = sugar
+            break
+        }
+        total = gb+cc+sugar
+        totalQty.innerHTML = total
+    })
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+}
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+
